8faef973b60eac10040cf9e2417ad53cb62cbcd3
jeremyrdavis <jeremyrdavis@mac.com>
2024-05-13 22:51:52 -0400

    fixed error

--- src/main/java/io/arrogantprogrammer/GreetingResource.java
+++ src/main/java/io/arrogantprogrammer/GreetingResource.java
@@ -4,17 +4,14 @@ import io.quarkus.logging.Log;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 @Path("/hello")
 public class GreetingResource {
 
-    private static List<Greeting> greetingList = List.of(
-            new Greeting("Jeremy", "Hello, Georgia Southern!"),
-            new Greeting("Roger", "Good morning!"),
-            new Greeting("Norman", "Hey, everyone!")
-    );
+    private List<Greeting> greetingList = new ArrayList<>();
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -27,6 +24,14 @@ public class GreetingResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Greeting addGreeting(Greeting greeting) {
+        if (greetingList == null || greetingList.isEmpty()) {
+            greetingList = new ArrayList() {
+                {
+                    add(new Greeting("Roger", "Hi, there!"));
+                    add(new Greeting("Norman", "Hey, everyone!"));
+                }
+            };
+        }
         Log.debugf("Adding greeting: %s from %s", greeting.author, greeting.text);
         greetingList.add(greeting);
         return new Greeting(greeting.author, greeting.text);
